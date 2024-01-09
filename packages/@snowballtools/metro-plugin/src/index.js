b4fabd644ce1d602b8c705a51088ff9d78026257
Eric Lewis <ericlewis777@gmail.com>
2024-01-09 16:55:38 -0500

    feat: metro plugin is greatly simplified

--- packages/@snowballtools/metro-plugin/src/index.js
+++ packages/@snowballtools/metro-plugin/src/index.js
@@ -1,48 +1,8 @@
-const path = require("path");
-
 module.exports = function (config) {
   config.resolver.extraNodeModules.crypto = require.resolve(
     "react-native-quick-crypto",
   );
   config.resolver.extraNodeModules.stream =
     require.resolve("stream-browserify");
-  config.resolver.resolveRequest = (
-    context,
-    moduleName,
-    platform,
-    moduleNameLookups,
-  ) => {
-    // Restrict to rewriting 'webcrypto.js' only when required by 'jose'
-    if (
-      moduleName.includes("webcrypto.js") &&
-      context.originModulePath.includes("/node_modules/jose/")
-    ) {
-      return {
-        type: "sourceFile",
-        filePath: path.resolve(__dirname, "webcrypto.js"),
-      };
-    }
-    // Restrict to rewriting 'nacl.js' only when required by '@lit-protocol/nacl'
-    if (moduleName === "@lit-protocol/nacl") {
-      return {
-        type: "sourceFile",
-        filePath: path.resolve(__dirname, "nacl.js"),
-      };
-    }
-	// Restrict to rewriting 'micro-ftch.js' only when required by 'micro-ftch'
-	if (moduleName === "micro-ftch") {
-		  return {
-			type: "sourceFile",
-			filePath: path.resolve(__dirname, "micro-ftch.js"),
-	 	 };	
-	}
-    // Fall back to Metro's default resolver for all other modules
-    return context.resolveRequest(
-      context,
-      moduleName,
-      platform,
-      moduleNameLookups,
-    );
-  };
   return config;
 };
--- packages/@snowballtools/metro-plugin/src/webcrypto.js
+++
@@ -1,6 +0,0 @@
-const c = require('crypto');
-module.exports = {
-  getRandomValues: c.getRandomValues || (() => {
-    console.log("getRandomValues is not supported")
-  })
-};
